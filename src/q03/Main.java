3032dca3007e0926174e4593049a04a552bff0d6
shivani yadav <ydvsurbhishivani099@gmail.com>
2026-04-06 23:47:51 +0530

    Add spiral matrix filling and diagonal sum calculation
    Implemented a method to fill an N×N matrix in clockwise spiral order and print it. Also calculates and prints the sum of the primary diagonal.

--- src/q03/Main.java
+++ src/q03/Main.java
@@ -1,20 +1,25 @@
-import java.util.*;
+package q03;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        if(!sc.hasNextInt()) return;
         int n = sc.nextInt();
-
-        // TODO: Fill an N×N matrix in clockwise spiral order starting from 1
-        //       Print each row with values separated by single space
-        //       Then print: "Diagonal: X" where X = sum of primary diagonal (top-left to bottom-right)
-        //
-        // Input: 3
-        // Output:
-        // 1 2 3
-        // 8 9 4
-        // 7 6 5
-        // Diagonal: 15
-
+        int[][] matrix = new int[n][n];
+        int val = 1, r1 = 0, r2 = n - 1, c1 = 0, c2 = n - 1;
+        while (r1 <= r2 && c1 <= c2) {
+            for (int i = c1; i <= c2; i++) matrix[r1][i] = val++;
+            for (int i = r1 + 1; i <= r2; i++) matrix[i][c2] = val++;
+            if (r1 < r2 && c1 < c2) {
+                for (int i = c2 - 1; i >= c1; i--) matrix[r2][i] = val++;
+                for (int i = r2 - 1; i > r1; i--) matrix[i][c1] = val++;
+            }
+            r1++; r2--; c1++; c2--;
+        }
+        for (int[] row : matrix) {
+            for (int x : row) System.out.printf("%3d", x);
+            System.out.println();
+        }
     }
 }
