0442f2274a0ffe322ba4681b78419302ee06814e
shivani yadav <ydvsurbhishivani099@gmail.com>
2026-04-06 23:51:13 +0530

    Implement Fibonacci sequence printing with input check
    Added input validation and implemented Fibonacci sequence printing.

--- src/q06/Main.java
+++ src/q06/Main.java
@@ -1,15 +1,18 @@
-import java.util.*;
+package q06;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        if(!sc.hasNextInt()) return;
         int n = sc.nextInt();
-
-        // TODO: Print first n Fibonacci numbers separated by spaces
-        //       F(0)=0, F(1)=1, F(n)=F(n-1)+F(n-2)
-        //
-        // Input: 8
-        // Output: 0 1 1 2 3 5 8 13
-
+        int a = 0, b = 1;
+        for (int i = 0; i < n; i++) {
+            System.out.print(a + " ");
+            int next = a + b;
+            a = b;
+            b = next;
+        }
+        System.out.println();
     }
 }
