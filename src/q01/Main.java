9682ae40ceae2a128affd5a3baf1fd68eeb7b673
shivani yadav <ydvsurbhishivani099@gmail.com>
2026-04-06 23:46:22 +0530

    Implement hollow diamond pattern printing
    Added logic to print a hollow diamond pattern based on user input.

--- src/q01/Main.java
+++ src/q01/Main.java
@@ -1,25 +1,26 @@
-import java.util.*;
+package q01;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        if(!sc.hasNextInt()) return;
         int n = sc.nextInt();
-
-        // TODO: Print a hollow diamond pattern of half-height n using '*'
-        //
-        // Rules:
-        //   - Only border cells have '*', inside is spaces
-        //   - No trailing spaces on any line
-        //
-        // Input: 4
-        // Output:
-        //    *
-        //   * *
-        //  *   *
-        // *     *
-        //  *   *
-        //   * *
-        //    *
-
+        for (int i = 1; i <= n; i++) {
+            for (int j = i; j < n; j++) System.out.print(" ");
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                if (j == 1 || j == (2 * i - 1)) System.out.print("*");
+                else System.out.print(" ");
+            }
+            System.out.println();
+        }
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = n; j > i; j--) System.out.print(" ");
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                if (j == 1 || j == (2 * i - 1)) System.out.print("*");
+                else System.out.print(" ");
+            }
+            System.out.println();
+        }
     }
 }
